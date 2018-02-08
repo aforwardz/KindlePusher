80d98513dd09502c7542be1e58f809ed742376f7
Aforwardz <aforwardzyu@gmail.com>
2018-02-08 17:44:57 +0800

    add ebook on server

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,4 +32,7 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
+//
+router.get('/ebook', controllers.ebook)
+
 module.exports = router
